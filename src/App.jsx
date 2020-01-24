b3ae1270f45fb78119ae62cbaa1b4372e1de3f1f
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-01-24 09:34:22 +0100

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import logo from './assets/LogoLight.svg';
 import './App.css';
 
+import Button from './components/Button';
 import Hamburger from './components/Hamburger';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Hamburger />
-        <img src={logo} alt="POLWEAR" height="50px" />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Hamburger />
+          <img src={logo} alt="POLWEAR" height="50px" />
+        </header>
+        <Switch>
+          <Route path="/" exact>
+            <Link to="/cart">
+              <Button>Cart</Button>
+            </Link>
+          </Route>
+          <Route path="/cart">
+            <Link to="/">
+              <Button>Home</Button>
+            </Link>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
