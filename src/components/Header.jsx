c77178b82efb6bd5358b4a61cb6a1ef5d434be54
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-02-01 11:56:54 +0100

    Add flex to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 import logo from '../assets/LogoLight.svg';
@@ -6,14 +7,22 @@ import logo from '../assets/LogoLight.svg';
 import CartButton from './CartButton';
 import Hamburger from './Hamburger';
 
+const HeaderWrapper = styled.header`
+  display: flex;
+  flex-direction: row;
+  padding: 20px 10%;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Header = () => (
-  <header className="App-header">
+  <HeaderWrapper>
     <Hamburger />
-    <Link to="/" style={{ margin: '0 20px' }}>
+    <Link to="/" style={{ flex: 1, margin: '0 20px' }}>
       <img src={logo} alt="POLWEAR" height="40vw" />
     </Link>
     <CartButton empty />
-  </header>
+  </HeaderWrapper>
 );
 
 export default Header;
